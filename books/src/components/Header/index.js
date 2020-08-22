2d62fc093479aaa008e0950989bb2cb977a4fb0e
Dania King <dania@fortunebuilders.com>
2020-08-21 22:48:51 -0700

    added main paged, routes, and external routes

--- books/src/components/Header/index.js
+++ books/src/components/Header/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBIcon, MDBContainer } from 'mdbreact';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 class Header extends React.Component
 {
@@ -25,28 +24,25 @@ class Header extends React.Component
         const container = { height: 100 }
         return (
             <div>
-                <Router>
-                    <header>
-                        <MDBNavbar className="purple-gradient" dark expand="md">
-                            <MDBNavbarBrand href="/">
-                                <strong>Google Books</strong>
-                            </MDBNavbarBrand>
-                            <MDBNavbarToggler onClick={this.onClick} />
-                            <MDBCollapse isOpen={this.state.collapse} navbar>
-                                <MDBNavbarNav right>
-                                    <MDBNavItem>
-                                        <MDBNavLink to="#"><MDBIcon fab icon="google" /></MDBNavLink>
-                                    </MDBNavItem><MDBNavItem>
-                                        <MDBNavLink to="#"><MDBIcon fab icon="google-play" /></MDBNavLink>
-                                    </MDBNavItem>
-                                    <MDBNavItem>
-                                        <MDBNavLink to="#"><MDBIcon fab icon="google-plus" /></MDBNavLink>
-                                    </MDBNavItem>
-                                </MDBNavbarNav>
-                            </MDBCollapse>
-                        </MDBNavbar>
-                    </header>
-                </Router>
+
+                <header>
+                    <MDBNavbar className="purple-gradient" dark expand="md">
+                        <MDBNavbarBrand href="/">
+                            <strong>Google Books</strong>
+                        </MDBNavbarBrand>
+                        <MDBNavbarToggler onClick={this.onClick} />
+                        <MDBCollapse isOpen={this.state.collapse} navbar>
+                            <MDBNavbarNav right>
+                                <MDBNavItem>
+                                    <MDBNavLink to="google.com"><MDBIcon fab icon="google" /></MDBNavLink>
+                                </MDBNavItem><MDBNavItem>
+                                    <MDBNavLink to="play.google.com"><MDBIcon fab icon="google-play" /></MDBNavLink>
+                                </MDBNavItem>
+                            </MDBNavbarNav>
+                        </MDBCollapse>
+                    </MDBNavbar>
+                </header>
+
                 <MDBContainer style={container} className="text-center mt-5">
                     <h2>Google Book Search</h2>
                     <h5>Find your piece of literary comfort</h5>
